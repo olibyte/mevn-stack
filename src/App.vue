53d95e23dd62bed341fc5bc796d90e9198654d07
Oliver <ocben1@gmail.com>
2019-08-06 01:48:09 +1000

    added footer component

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,18 @@
     <div id="app-container">
       <router-view/>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
   import Navbar from '@/components/Navbar.vue'
-
+  import Footer from '@/components/Footer.vue'
   export default {
     name: 'app',
     components: {
-      Navbar
+      Navbar,
+      Footer
     }
   }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+<footer id="custom-footer">
+    <span class="mr-4">&copy; 2019 Web Cretin</span>
+    <span class="mr-4">Privacy</span>
+    <span>Terms of Service</span>
+</footer>
+</template>
