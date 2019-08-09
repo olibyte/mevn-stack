4f85f0130d8596a8ce8affe7aba1715905f199be
Oliver <ocben1@gmail.com>
2019-08-09 22:33:13 +1000

    router config

--- src/router.js
+++ src/router.js
@@ -47,14 +47,6 @@ export default new Router({
       path: '*',
       redirect: '/'
     }
-    //{
-    //  path: '/about',
-    //  name: 'about',
-    //  // route level code-splitting
-    //  // this generates a separate chunk (about.[hash].js) for this route
-    //  // which is lazy-loaded when the route is visited.
-    //  component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    //}
   ],
   linkActiveClass: 'active',
   mode: 'history'
