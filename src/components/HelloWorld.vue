e8de3b61798a9ad9a69220db64b98a49dc64320d
Oliver <ocben1@gmail.com>
2019-08-11 22:44:16 +1000

    update titles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="jumbotron custom-bg-dark">
-  <h1 class="display-4">MEVN Stack Task Manager</h1>
+  <h1 class="display-4">MEVN Task Manager</h1>
   <p class="lead">This is a sample task manager built with MongoDB, ExpressJS, VueJS, and NodeJS technologies.</p>
   <hr class="my-4">
   <p>Click below to begin managing tasks for users.</p>
