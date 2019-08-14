36fb73feae4e660a9eedc76baad460b6e6e370bc
Oliver <ocben1@gmail.com>
2019-08-15 07:57:04 +1000

    authservice functions

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -3,3 +3,11 @@ import store from '../store';
 export function isLoggedIn() {
     
 }
+
+export function login() {
+
+}
+
+function setToken(token) {
+    localStorage.setItem('token', token); //pass in key and value
+}
