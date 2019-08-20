ba088a7feedf649b7ab4fd7e39f4048be2de6c7e
Oliver <ocben1@gmail.com>
2019-08-20 21:31:53 +1000

    vuex state

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,14 +1,26 @@
 import store from '../store';
 
 export function isLoggedIn() {
-    
+    const token = localStorage.getItem('token');
+    return token != null;
 }
 
 export function login() {
-
+    const token = {
+        username: 'oliver'
+    }
+    setToken(token);
 }
 
 function setToken(token) {
-    localStorage.setItem('token', token); //pass in key and values.
+    localStorage.setItem('token', JSON.stringify(token)); //pass in key and values.
     store.dispatch('authenticate');
+}
+
+export function getUsername() {
+    return 'oliver';
+}
+
+export function getUserId() {
+    return 1;
 }
