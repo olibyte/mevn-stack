c14148113d09be18573b81493f6d21a69ab9e01d
Oliver <ocben1@gmail.com>
2019-08-22 17:30:13 +1000

    add login form

--- src/views/authentication/Login.vue
+++ src/views/authentication/Login.vue
@@ -1,3 +1,21 @@
 <template>
+<div>
     <h1>Login Route</h1>
+    <form class="custom-form">
+  <div class="form-group">
+    <label for="username">Username</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
 </template>
