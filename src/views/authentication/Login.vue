d3d6dec03f8862f4c931100c9632da247f1882be
Oliver Bennett <oliver.bennett@unimelb.edu.au>
2019-08-23 12:57:21 +1000

    login update

--- src/views/authentication/Login.vue
+++ src/views/authentication/Login.vue
@@ -4,7 +4,7 @@
     <form class="custom-form">
   <div class="form-group">
     <label for="username">Username</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="text" class="form-control" id="username" placeholder="username">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
