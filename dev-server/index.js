499cf004239009464853f2ade5171b8dcad4818c
Oliver <ocben1@gmail.com>
2019-08-30 22:09:57 +1000

    add express dev and prod servers

--- dev-server/index.js
+++ dev-server/index.js
@@ -1,9 +1,10 @@
 //nodemon is a tool that helps develop node.js based applications by automatically restarting the node 
 //application when file changes in the directory are detected.
-const express = require('express')
+//const express = require('express')
+import express from'express';
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`MEVN app listening on port ${port}!`))
---
+++ prod-server/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)(); //nodemon is a tool that helps develop node.js based applications by automatically restarting the node 
+//application when file changes in the directory are detected.
+//const express = require('express')
+
+var port = 3000;
+
+app.get('/', function (req, res) {
+  return res.send('Hello World!');
+});
+
+app.listen(port, function () {
+  return console.log('MEVN app listening on port ' + port + '!');
+});
