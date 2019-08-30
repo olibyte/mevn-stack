8ae10c61c97b0c69235ace957d6f7778594bc1cf
Oliver <ocben1@gmail.com>
2019-08-30 21:37:03 +1000

    added dev server and nodemon

---
+++ dev-server/index.js
@@ -0,0 +1,9 @@
+//nodemon is a tool that helps develop node.js based 
+//applications by automatically restarting the node application when file changes in the directory are detected.
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
