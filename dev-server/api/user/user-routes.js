f6c42cc7e2c95e5894efbf8bd082587165bc4fb9
Oliver <ocben1@gmail.com>
2019-09-02 07:35:39 +1000

    add routing endpoints

---
+++ dev-server/api/user/user-routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/user', (req, res) => {
+    res.send('get.user - get all users');
+});
+
+
+export default router;
--- dev-server/routes.js
+++ dev-server/routes.js
@@ -1,5 +1,11 @@
 import taskRoutes from './api/task/tasks-routes';
+import regRoutes from './api/register/register-routes'
+import authRoutes from './api/auth/auth-routes'
+import userRoutes from './api/user/user-routes'
 
 export function registerRoutes(app) {
     app.use('/api', taskRoutes);
+    app.use('/api', regRoutes);
+    app.use('/api', authRoutes);
+    app.use('/api', userRoutes);
 }
