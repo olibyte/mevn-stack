99decab550c898de7874b00a4f21709a7465da3d
Oliver <ocben1@gmail.com>
2019-09-06 07:54:43 +1000

    to do add temp script

--- dev-server/config/env.js
+++ dev-server/config/env.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 export function setEnvironments(app) {
     if (process.env.NODE_ENV !== 'production') { //if we're in development mode...
@@ -11,9 +12,13 @@ export function setEnvironments(app) {
 }
 function setDevEnv(app) {
     console.log("setting the development environment");
+    app.use(bodyParser.json);
     app.use(morgan('dev'));
     app.use(cors());
 }
 function setProdEnv(app) {
+    app.use(bodyParser.json);
+    app.use(express.static(__dirname + '/../dist')); //take build folder and serve it as static content
     console.log("setting the production environment");
+
 }
