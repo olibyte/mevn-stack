6058a3fc14fdfe859a6f0bf8e52ade47e966bb9d
Oliver <ocben1@gmail.com>
2019-09-11 07:53:51 +1000

    auth-controller

--- dev-server/api/auth/auth-controller.js
+++ dev-server/api/auth/auth-controller.js
@@ -1,7 +1,24 @@
 import { StringUtil} from '../../utilities/string-util';
 
 export function index(req, res) {
-
+    const validation =  validateIndex(req.body);
+    if(!validation.isValid) {
+        return res.status(400).json({ message: validation.message });
+    }
+    return res.status(204).json();
 }
 
-function validateIndex();
+function validateIndex(body) {
+    let errors = '';
+
+    if(StringUtil.isEmpty(body.username)) {
+        errors += 'Username is required.' ;
+    }
+    if(StringUtil.isEmpty(body.password)) {
+        errors += 'Password is required' ;
+    }
+    return {
+        isValid: StringUtil.isEmpty(errors),
+        message: errors
+    }
+};
