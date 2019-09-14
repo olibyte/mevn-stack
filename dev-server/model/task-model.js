7698d185ff27ade8539a948652f56a13d785731b
Oliver <ocben1@gmail.com>
2019-09-14 10:02:09 +1000

    task-model

--- dev-server/model/task-model.js
+++ dev-server/model/task-model.js
@@ -1,1 +1,11 @@
 import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    dueDate: { type: Date, default: Date.now },
+    completed: { type: Boolean, default: false },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user' }
+});
+
+export default mongoose.model('task', taskSchema);
