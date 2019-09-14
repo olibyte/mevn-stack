c74aca1bf63073bf4966cd924d9b0538931eed5d
Oliver <ocben1@gmail.com>
2019-09-15 00:04:07 +1000

    update tasks controller

--- dev-server/api/task/tasks-controller.js
+++ dev-server/api/task/tasks-controller.js
@@ -1,10 +1,37 @@
-export function index(req,res) {
+import User from '../../model/user-model';
+import Task from '../../model/task-model';
+import taskModel from '../../model/task-model';
+import moment from 'moment';
+
+export function index(req, res) {
     //FIND ALL TASKS
+    Task.find({}, (error, tasks) => {
+        if (error) {
+            return res.status(500).json();
+        }
+        return res.status(200).json({ tasks: tasks});
+    }).populate('author', 'username', 'user');
     return res.status(200).json();
 }
+
 export function create(req,res) {
     //CREATE TASK
-    return res.status(201).json(); //201 whenever we create
+    const id = 10;
+    User.findOne({ _id: id}, (error, user) => {
+        if(error && !user) {
+            return res.status(500).json();
+        }
+        const task = new Task(req.body.task);
+        task.author = user._id;
+        task.dueDate = moment(task.dueDate);
+
+        task.save(error => {
+            if (error) {
+                return res.status(500).json();
+            }
+            return res.status(201).json();
+        });
+    });
 }
 export function update(req,res) {
     //UPDATE TASK
@@ -16,5 +43,14 @@ export function remove(req,res) {
 }
 export function show(req,res) {
     //GET TASK BY ID
+    Task.findOne({ _id: req.params.id}, (error, tasks) => { 
+        if(error) {
+            return res.status(500).json();
+        }
+        if(!task) {
+            return res.status(404).json(); 
+        }
+        return res.status(200).json({ task: task });
+    });
     return res.status(200).json();
 }
