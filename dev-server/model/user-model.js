2f599f0689c821664d7d1672a7e5e1e4213c834e
Oliver <ocben1@gmail.com>
2019-09-15 23:18:48 +1000

    axios for validation

--- dev-server/model/user-model.js
+++ dev-server/model/user-model.js
@@ -14,6 +14,9 @@ userSchema.virtual('fullName').get(function() {
     const last = StringUtil.capitalize(this.last.toLowerCase());
     return `${first} ${last}`;
 });
+userSchema.statics.passwordMatches = function(password, hash) {
+    return bcrypt.compareSync(password, hash);
+}
 userSchema.pre('save', function(next) {
     this.username = this.username.toLowerCase(); //convert all to lowercase for consistency in db
     this.first = this.first.toLowerCase();
